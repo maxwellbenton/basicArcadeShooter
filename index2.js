73d570193a4607b7c54b6323068a27621504dc4e
Maxwell Benton <maxwellbenton@gmail.com>
2017-05-25 16:27:42 -0400

    collision working

--- index2.js
+++ index2.js
@@ -3,21 +3,32 @@ var ctx = canvas.getContext("2d");
 var playerHeight = 10;
 var playerWidth = 10;
 var playerX = (canvas.width-playerWidth)/2;
-var bulletRadius = 3;
+var enemyRadius = 25;
 var fireRateMax = 10;
 var fireNum = 0;
+var spawnRate = 50;
+var spawnTick = 0;
+var score = 0;
 
 var rightPressed = false;
 var leftPressed = false;
 var spacebarPressed = false;
+var bulletRadius = 3;
 var bullets = [];
+var enemies = [];
 var by = -4;
+var ey = 3;
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.getElementById('myCanvas').style.background = "none"
 document.getElementById('myCanvas').style.zIndex = 100;
 
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD"
+    ctx.fillText("Score: "+score, 8,20);
+}
 function keyDownHandler(e) {
     if(e.keyCode == 39) {
         rightPressed = true;
@@ -41,7 +52,7 @@ function keyUpHandler(e) {
 }
 
 function createBullet() {
-    console.log("create bullet")
+    
     var bb = new Object();
     bb.x = playerX;
     bb.y = canvas.height - 5;
@@ -56,15 +67,61 @@ function drawBullet(x,y) {
     ctx.closePath();
 }
 
+function createEnemy() {
+    
+    var enemy = new Object();
+    enemy.x = Math.random()*canvas.width;
+    
+    enemy.rad = parseInt(Math.random()*50)+10;
+    enemy.y = 0-(enemy.rad*2);
+    enemy.health = 4;
+    enemies.push(enemy)
+}
+
+function drawEnemy(x,y,rad) {
+    ctx.beginPath();
+    ctx.arc(x, y, rad, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
 function drawPlayer() {
     ctx.beginPath();
-    ctx.rect(playerX, canvas.height-playerHeight, playerWidth, playerHeight);
+    ctx.arc(playerX, canvas.height-playerHeight, playerWidth/2, 0, Math.PI*2);
    
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
+function collisionDetection() {
+    enemies.forEach(function(en, enI) {
+        bullets.forEach(function(bb,bbI){    
+            var dx = bb.x - en.x;
+            var dy = bb.y - en.y;
+            var distance = Math.sqrt(dx * dx + dy * dy);
+            
+            if (distance < bulletRadius + en.rad) {
+                en.health -= 1;
+                if(en.health === 0) {
+                    score++;    
+                    enemies.splice(enI,1);
+                }
+                bullets.splice(bbI,1);                
+            }
+        })
+        var dx = playerX - en.x;
+        var dy = canvas.height-playerWidth/2 - en.y;
+        var distance = Math.sqrt(dx * dx + dy * dy);
+        
+        if (distance < (en.rad + (playerWidth/2))) {
+            console.log("dx:"+dx+", dy:"+dy+", enY:"+en.y+", dist:"+distance+", enR:"+en.rad+", pR:"+(playerWidth/2));
+            alert("GAME OVER");
+            document.location.reload();
+        }
+    })
+}
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     bullets.forEach(function(bb,index) {
@@ -75,6 +132,15 @@ function draw() {
             bb.y += by;
         }
     })
+    enemies.forEach(function(en, index) {
+        drawEnemy(en.x,en.y,en.rad);
+        if(en.y>canvas.height+(en.rad*2)) {
+            enemies.splice(index,1)
+        } else {
+            en.y += ey;
+        }
+    })
+    
     // console.log(bullets.length)
 
     // if(rightPressed && playerX < canvas.width-playerWidth) {
@@ -84,6 +150,8 @@ function draw() {
     //     playerX -= 7;
     // } 
     drawPlayer();
+    drawScore();
+    collisionDetection();
     if(rightPressed && playerX < canvas.width-playerWidth) {
         playerX += 7;
     }
@@ -96,8 +164,12 @@ function draw() {
             createBullet();
             fireNum = 0;
         }
-
     }
+    if(spawnTick >= spawnRate) {
+        createEnemy();
+        spawnTick = 0;
+    }
+    spawnTick += 1;
     requestAnimationFrame(draw);
 }
 
